5b443ea7c3335a6716abeb5b5ba12faa4bbe1ee0
unstoppableayush <ayushkum884@gmail.com>
2024-05-15 14:48:09 +0530

    text added

--- src/pages/Homepage/Hero.jsx
+++ src/pages/Homepage/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
       backgroundImage: `url(https://firebasestorage.googleapis.com/v0/b/gais-f3e13.appspot.com/o/GIF%2FHomepage%2Fimg1_home.png?alt=media&token=c8768038-0f1e-40be-b354-26c595480657)`,
     }}
   >
-    <h2 className="md:p-24 p-5 pt-24 md-pt-0 text-3xl md:text-6xl lg:text-7xl font-bold tracking-wide leading-10  w-1/2">IT'S A PLATFORM WHERE GETS DONE NOT JUST DISCUSSED.</h2>
+    <h2 className="md:p-24 p-5 pt-24 md-pt-0 text-3xl md:text-6xl lg:text-7xl font-bold tracking-wide leading-10  w-1/2">IT'S A PLATFORM WHERE BUSINESS GETS DONE NOT JUST DISCUSSED.</h2>
   </section>
   )
 }
